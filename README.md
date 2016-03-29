a8eb0ed267ecc012c9f3d4210c04c491a6388c58
roby <robyremzy@openmailbox.org>
2016-03-29 15:27:26 +0200

    version 0.01-dev

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Cozy-Map
-Carto projects, Cozy friendly, using open Street Map data.
+
+Simple package of Cozy-sdk combined with Leaflet JavaScript library.
+
+- NPM leaflet package version: "0.7.7"
+- [Leaflet doc](http://leafletjs.com)
+- [Leaflet on GutHub](https://github.com/Leaflet/Leaflet)
+
+
+### Map
+map tiles are provided by Open Street and ESRI Map in https.
+
+### Geocoder
+search are https request from [OSM/Nominatim](http://wiki.openstreetmap.org/wiki/Nominatim) and include also reverse geocoding `latitude longitude`.  
+why there is no auto-completion [?](http://wiki.openstreetmap.org/wiki/Nominatim_usage_policy#Unacceptable_Use)  
+For auto-completion there is a possibility to set up your own [OSRM server](https://github.com/Project-OSRM/osrm-backend/wiki/Running-OSRM)  
+or use API such as [Mapzen](https://mapzen.com/projects/valhalla/) or [Mapbox's](https://www.mapbox.com/developers/api/directions/) geocoding services. Both of them are doing great things for Open street Map. Here for more informations about the Leaflet Routing Machine [LRM](http://www.liedman.net/leaflet-routing-machine/api/).
+
+### Location
+can be switch on/off.
+
+### Hash address bar
+update your position with the `#zomm/latitude/longitude` of the center of the map.  
+
+ex: `http://localhost:9505/#3/46.86/3.87`  
+***should be able to update your position by enter a new position directly inside the address bar.***
+
+
+
+## Fork & Mod
+check `client/src/app.js` and `client/public/styles.css`  
+Discussion on the [Cozy.io/Forum](https://forum.cozy.io/t/app-leaflet-map-here-i-am-there-you-go/2114)
+
+
+## What is Cozy?
+
+![Cozy Logo](https://raw.github.com/cozy/cozy-setup/gh-pages/assets/images/happycloud.png)
+
+[Cozy](http://cozy.io) is a platform that brings all your web services in the
+same private space.  With it, your web apps and your devices can share data
+easily, providing you
+with a new experience. You can install Cozy on your own hardware where no one
+profiles you.
+
+## Community
+
+You can reach the Cozy Community by:
+
+* Chatting with us on IRC #cozycloud on irc.freenode.net
+* Posting on our [Forum](https://forum.cozy.io/)
+* Posting issues on the [Github repos](https://github.com/cozy/)
+* Mentioning us on [Twitter](http://twitter.com/mycozycloud)
---
+++ client/src/app.js
@@ -0,0 +1,179 @@
+// require modules
+var L = require('leaflet');
+require('./leaflet-routing-machine.js');
+require('./Control.Geocoder.js');
+require('./leaflet.MiniMap.js');
+require('./leaflet.Locate.js');
+require('./leaflet-sidebar.js');
+require('./leaflet.Hash.js');
+// specify the path to the leaflet images folder
+L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/';
+
+
+
+//disable zoomControl (which is topleft by default) when initializing map&options
+var map = new L.map('map', {
+  attributionControl: false,
+  zoomControl: false
+});
+
+
+
+// set map url tiles layer
+var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+// set OpenStreetMap attribution
+var osmAttrib = " ";
+
+// set Map tiles layer and Options
+var osm = new L.TileLayer(osmUrl, {
+  minZoom: 1,
+  maxZoom: 19,
+  detectRetina: true,
+});
+
+// add the tile layer to the map
+map.addLayer(osm);
+
+// set the position and zoom level of the map
+map.setView(new L.LatLng(46.8, 3.8),3);
+
+
+
+// icon for the routing machine
+var startRicon = L.icon({
+    iconUrl: 'public/images/pinstart.png',
+    iconRetinaUrl: 'public/images/pinstart.png',
+    iconSize: [36, 47],
+    iconAnchor: [18, 47],
+    popupAnchor: [0, -48],
+});
+var endRicon = L.icon({
+    iconUrl: 'public/images/pinend.png',
+    iconRetinaUrl: 'public/images/pinend.png',
+    iconSize: [36, 47],
+    iconAnchor: [18, 47],
+    popupAnchor: [0, -48],
+});
+
+
+
+// Routing machine features
+var sidebarlrm = L.Routing.control({
+  plan: L.Routing.plan(null, {
+      createMarker: function(i, startwp) {
+        return L.marker(startwp.latLng, {
+          draggable: true,
+          icon: startRicon
+        });
+      },
+      geocoder: L.Control.Geocoder.nominatim(),
+      routeWhileDragging: true,
+      reverseWaypoints: true,
+      draggable: true
+    }),
+    position: 'topleft',
+    routeWhileDragging: true,
+    routeDragTimeout: 250,
+    draggableWaypoints:true
+});
+// include the routing machine into the sidebar
+var lrmBlock = sidebarlrm.onAdd(map);
+document.getElementById('sidebarlrm').appendChild(lrmBlock);
+
+
+
+// Stand alone Geocoder features
+var geocoder = L.Control.geocoder({
+  position: 'topleft',
+  collapsed: false,
+  placeholder: 'Drop a marker',
+  errorMessage: '‘X’ never, ever marks the spot.'
+});
+// include the geocoder into the sidebar
+var gecBlock = geocoder.onAdd(map);
+document.getElementById('sidebarex').appendChild(gecBlock);
+
+
+
+// MiniMap layer Options
+var esriUrl='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
+
+// set MiniMap attribution
+var esriAttrib='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+
+// set MiniMap features
+var esri = new L.TileLayer(esriUrl, {
+  minZoom: 0,
+  maxZoom: 11,
+  attribution: esriAttrib
+});
+
+// set MiniMap on map
+var miniMap = new L.Control.MiniMap(esri, {
+  position: 'bottomright',
+  width: 80,
+  height: 80
+}).addTo(map);
+
+
+
+//add zoom control with your options
+L.control.zoom({
+  position:'bottomright'
+}).addTo(map);
+
+
+
+// icon for locate
+var markerLicon = {
+    iconUrl: 'public/images/bluedot.png',
+    iconSize: [17, 17],
+    iconAnchor: [9, 9],
+    popupAnchor: [0, -10],
+    labelAnchor: [3, -4],
+};
+
+// locate controle on the top right side
+L.control.locate(
+  {
+  position: 'topright',
+  icon: 'fa fa-location-arrow',
+  iconLoading: 'fa fa-refresh fa-spin',
+  drawCircle: true,
+  circlePadding: [20, 20],
+  circleStyle: {
+    color: "#FFF",
+    fillColor: "#000",
+    fillOpacity: "0.1",
+    weight: "2",
+  },
+  follow: true,
+  markerClass: L.marker,
+  markerStyle: {
+    icon: L.icon( markerLicon ),
+    className: 'locatemarker-pulsate',
+  },
+  metric: true,
+  strings: {
+    title: "Show me where I am",
+    metersUnit: "meters",
+    feetUnit: "feet",
+    popup: "<center>You are around " + "{distance} {unit} " + "from this point</center>",
+    outsideMapBoundsMsg: "You seem located outside the boundaries of the map"
+    },
+  locateOptions: {
+    enableHighAccuracy: true,
+    maxZoom: 20
+  }
+}).addTo(map);
+
+
+
+// spawn the sidebar on map
+var sidebar = L.control.sidebar('sidebar').addTo(map);
+
+
+
+// hash the address bar with the {./#ZOOM/LAT/LNG} center of the map
+var addresshash = new L.Hash(map);
